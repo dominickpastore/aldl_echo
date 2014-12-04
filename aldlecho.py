adc18f204704325e9b8511b56637986864df8769
Dominick C. Pastore <dominickpastore@dcpx.org>
2014-12-04 02:11:26 -0500

    Write send and receive code
    Add code for send and receive messages. This includes checksum
    generation and verification.
    
    Also add some basic echo abilities so I can test things so far tomorrow.

--- aldlecho.py
+++ aldlecho.py
@@ -25,10 +25,107 @@
 #
 
 import sys
+from time import sleep
 import serial
 
+# From here on, "s" is always the serial object for the ALDL link.
+
+def message_recv(s):
+    """Wait for a valid message, then return its contents
+
+    Wait for either an 0xf4 or an 0xf5. When it comes, read the rest of the
+    message and verify its checksum. If all checks out, then return the message
+    as an (id, mode, message) tuple. Otherwise, try again.
+    """
+    while True:
+        msg_id = None
+        msg_len = None
+        msg_mode = None
+        msg_body = None
+        
+        while msg_id != 0xf4 and msg_id != 0xf5:
+            msg_id = s.read()[0]
+
+        checksum = msg_id
+        
+        msg_len = s.read()[0]
+        checksum += msg_len
+        msg_len -= 0x56
+        
+        msg_mode = s.read()[0]
+        checksum += msg_mode
+
+        msg_body = s.read(msg_len)
+        for b in msg_body:
+            checksum += b
+
+        checksum_in = s.read()[0]
+
+        if checksum != checksum_in:
+            print("Warning: Checksum fail. Msg: {:02x} {:02x} {:02x}".format(
+                msg_id, msg_len + 0x56, msg_mode), end="", file=sys.stderr)
+            for b in msg_body:
+                print(" {:02x}".format(b), end="", file=sys.stderr)
+            print(" {:02x}".format(checksum_in), file=sys.stderr)
+        else:
+            return (msg_id, msg_mode, msg_body)
+
+def message_send(s, msg_id, msg_mode, msg_body):
+    """Send a message to the ECM.
+
+    Message length and checksum are calculated automatically. See the data
+    stream definition for what the parameters mean.
+
+    Arguments:
+    s: An open serial object
+    msg_id: An integer representing the message ID
+    msg_mode: An integer representing the message mode
+    msg_body: A bytes or bytearray object containing the message body
+    """
+    msg = bytearray()
+    msg.append(msg_id)
+    checksum = msg_id
+
+    msg_len = 0x56 + len(msg_body)
+    msg.append(0x56 + len(msg_body))
+    checksum = (checksum + msg_len)
+
+    msg.append(msg_mode)
+    checksum = (checksum + msg_mode)
+
+    msg += msg_body
+    for b in msg_body:
+        checksum = (checksum + b)
+
+    # Checksum is 1's compliment of sum of all bytes in the message
+    msg.append(~checksum & 0xff)
+
+    s.write(msg)
+
+# This function should be called with an open serial object. It starts with
+# the actual communication and echoing.
 def parse_stream(s):
-    pass
+    #s.write(b'\xf4\x57\x01\x00\xb4')
+    message_send(s, 0xf4, 0x01, b'\x00')
+    #message_send(s, 0xf5, 0x01, b'\x00')
+    #message_send(s, 0xf5, 0x01, b'\x01')
+
+    try:
+        while True:
+            #sleep(0.2)
+            #message_send(s, 0xf4, 0x01, b'\x00')
+            #print(message_recv(s))
+
+            #sleep(0.2)
+            #message_send(s, 0xf5, 0x01, b'\x00')
+            #print(message_recv(s))
+
+            #sleep(0.2)
+            #message_send(s, 0xf5, 0x01, b'\x01')
+
+            print(message_recv(s))
+    except KeyboardInterrupt:
+        print("Stopping...", file=sys.stderr)
 
 def main(serial_port):
     try:
